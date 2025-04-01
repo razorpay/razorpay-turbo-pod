4a4d60f6882c62f668f014312908a8437187674b
ramprasadAnand <ramprasad.a@razorpay.com>
2025-04-01 20:00:34 +0530

    Handled implementing the standard and custom checkout pods in the same repository

---
+++ razorpay-turbo/Example/Pods/Target Support Files/razorpay-turbo-ui-pod/razorpay-turbo-ui-pod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_razorpay_turbo_ui_pod : NSObject
+@end
+@implementation PodsDummy_razorpay_turbo_ui_pod
+@end
---
+++ razorpay-turbo/Example/razorpay-turbo-ui-pod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  razorpay-turbo-ui-pod
+//
+//  Created by 85918255 on 03/07/2024.
+//  Copyright (c) 2024 85918255. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
